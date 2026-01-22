e03de26812d4b3a537192cff573187d6d85247aa
Michał Świątek <michau.swiatek@gmail.com>
2026-01-22 14:16:33 +0100

    Add Gutenberg register form assets

--- dist/gutenberg/register-form/render.php
+++ dist/gutenberg/register-form/render.php
@@ -33,37 +33,56 @@ $consent_text = $attributes['consent_text'] ?? '';
     </div>
     <div class="form__row">
         <div class="form__group js-per">
-            <label class="form__label" for="firstname"><?php esc_html_e( 'Imię', 'netivo' ); ?></label>
-            <input required data-pristine-required-message="To pole jest wymagane" class="form__input"
+            <label class="form__label" for="company_name"><?php esc_html_e( 'Nazwa firmy', 'netivo' ); ?></label>
+            <input required data-pristine-required-message="<?php esc_attr_e( 'To pole jest wymagane', 'netivo' ); ?>" class="form__input"
                    type="text"
-                   name="first_name" id="firstname" placeholder="<?php esc_attr_e( 'Imię', 'netivo' ); ?>"/>
+                   name="company_name" id="company_name" placeholder="<?php esc_attr_e( 'Nazwa firmy', 'netivo' ); ?>"/>
         </div>
         <div class="form__group js-per">
-            <label class="form__label" for="email"><?php esc_html_e( 'Adres e-mail', 'netivo' ); ?></label>
-            <input required data-pristine-required-message="To pole jest wymagane"
-                   data-pristine-email-message="W tym polu musisz podać prawidłowy adres email"
-                   class="form__input" type="email" name="email" id="email"
-                   placeholder="<?php esc_attr_e( 'Adres e-mail', 'netivo' ); ?>"/>
+            <label class="form__label" for="nip"><?php esc_html_e( 'NIP', 'netivo' ); ?></label>
+            <input required data-pristine-required-message="<?php esc_attr_e( 'To pole jest wymagane', 'netivo' ); ?>"
+                   class="form__input" type="text" name="nip" id="nip"
+                   placeholder="<?php esc_attr_e( 'NIP', 'netivo' ); ?>"/>
         </div>
     </div>
     <div class="form__row">
-        <div class="form__group form__group-100 js-per">
-            <label class="form__label"
-                   for="message-title"><?php esc_html_e( 'Zapytanie do sklepu', 'netivo' ); ?></label>
-            <input class="form__input" type="text" name="message_title" id="message-title" required
-                   data-pristine-required-message="To pole jest wymagane"
-                   placeholder="<?php esc_attr_e( 'Zapytanie do sklepu', 'netivo' ); ?>"/>
+        <div class="form__group js-per">
+            <label class="form__label" for="first_name"><?php esc_html_e( 'Imię', 'netivo' ); ?></label>
+            <input required data-pristine-required-message="<?php esc_attr_e( 'To pole jest wymagane', 'netivo' ); ?>" class="form__input"
+                   type="text"
+                   name="first_name" id="first_name" placeholder="<?php esc_attr_e( 'Imię', 'netivo' ); ?>"/>
+        </div>
+        <div class="form__group js-per">
+            <label class="form__label" for="last_name"><?php esc_html_e( 'Nazwisko', 'netivo' ); ?></label>
+            <input required data-pristine-required-message="<?php esc_attr_e( 'To pole jest wymagane', 'netivo' ); ?>"
+                   class="form__input" type="text" name="last_name" id="last_name"
+                   placeholder="<?php esc_attr_e( 'Nazwisko', 'netivo' ); ?>"/>
+        </div>
+    </div>
+    <div class="form__row">
+        <div class="form__group js-per">
+            <label class="form__label" for="email"><?php esc_html_e( 'Adres e-mail', 'netivo' ); ?></label>
+            <input required data-pristine-required-message="<?php esc_attr_e( 'To pole jest wymagane', 'netivo' ); ?>"
+                   data-pristine-email-message="<?php esc_attr_e( 'W tym polu musisz podać prawidłowy adres email', 'netivo' ); ?>"
+                   class="form__input" type="email" name="email" id="email"
+                   placeholder="<?php esc_attr_e( 'Adres e-mail', 'netivo' ); ?>"/>
+        </div>
+        <div class="form__group js-per">
+            <label class="form__label" for="phone"><?php esc_html_e( 'Telefon', 'netivo' ); ?></label>
+            <input required data-pristine-required-message="<?php esc_attr_e( 'To pole jest wymagane', 'netivo' ); ?>" class="form__input"
+                   type="text"
+                   name="phone" id="phone" placeholder="<?php esc_attr_e( 'Telefon', 'netivo' ); ?>"/>
         </div>
     </div>
     <div class="form__group form__group-100 js-per">
-        <label class="form__label" for="message"><?php esc_html_e( 'Treść wiadomości', 'netivo' ); ?></label>
+        <label class="form__label" for="message"><?php esc_html_e( 'Wiadomość', 'netivo' ); ?></label>
         <textarea class="form__input form__input--textarea" required
-                  data-pristine-required-message="To pole jest wymagane" rows="10" name="message" id="message"
-                  placeholder="Treść wiadomości"></textarea>
+                  data-pristine-required-message="<?php esc_attr_e( 'To pole jest wymagane', 'netivo' ); ?>" rows="10" name="message" id="message"
+                  placeholder="<?php esc_attr_e('Dlaczego checsz założyć konto B2B?', 'netivo'); ?>"></textarea>
     </div>
     <div class="form__group form__group-100 js-per">
         <div class="form__row-checkbox">
-            <input required data-pristine-required-message="To pole jest wymagane" id="daneOsobowe" type="checkbox"
+            <input required data-pristine-required-message="<?php esc_attr_e( 'To pole jest wymagane', 'netivo' ); ?>" id="daneOsobowe" type="checkbox"
                    name="agree" class="form__input-checkbox"/>
             <label for="daneOsobowe" class="form__label-checkbox">
 				<?php echo wp_kses_post( sprintf( $consent_text, ( esc_url( get_privacy_policy_url() ) ) ) ); ?>
--- sources/gutenberg/register-form/front/script/index.js
+++ sources/gutenberg/register-form/front/script/index.js
@@ -1,106 +1,121 @@
-import Pristine from "pristinejs";
-import Form from "@netivo/base-scripts/javascript/form";
+import Pristine from 'pristinejs';
+import Form     from '@netivo/base-scripts/javascript/form';
 
-let contact_form = document.querySelector( ".js-contact-form" );
+let contact_form = document.querySelector( '.js-contact-form' );
 if ( contact_form !== null ) {
   let loader = contact_form.querySelector( '[data-element="loader"]' );
-  let recaptcha_key = contact_form.getAttribute( "data-recaptcha" );
+  let recaptcha_key = contact_form.getAttribute( 'data-recaptcha' );
   let pristine = new Pristine( contact_form, {
-    classTo:         "js-per",
-    errorTextParent: "js-per"
+    classTo: 'js-per', errorTextParent: 'js-per',
   } );
 
   new Form( contact_form, {
-    action:       "/wp-json/netivo/v1/b2b-form",
-    data:         ( el ) => {
+    action:          '/wp-json/netivo/v1/b2b-form', data: ( el ) => {
       return new Promise( ( resolve ) => {
         let data = {
-          first_name:    "",
-          email:         "",
-          message_title: "",
-          message:       "",
-          nonce:         "",
-          recaptcha:     ""
+          company_name: '',
+          nip:          '',
+          first_name:   '',
+          last_name:    '',
+          email:        '',
+          phone:        '',
+          message:      '',
+          agree:        '',
+          nonce:        '',
+          recaptcha:    '',
         };
 
-        let form_first_name = el.querySelector( '[name="first_name"]' ),
-          form_email = el.querySelector( '[name="email"]' ),
-          form_message_title = el.querySelector( '[name="message_title"]' ),
-          form_message = el.querySelector( '[name="message"]' ),
-          form_nonce = el.querySelector( '[name="nonce"]' );
+        let form_company_name = el.querySelector( '[name="company_name"]' ),
+            form_nip          = el.querySelector( '[name="nip"]' ),
+            form_first_name   = el.querySelector( '[name="first_name"]' ),
+            form_last_name    = el.querySelector( '[name="last_name"]' ),
+            form_email        = el.querySelector( '[name="email"]' ),
+            form_phone        = el.querySelector( '[name="phone"]' ),
+            form_message      = el.querySelector( '[name="message"]' ),
+            form_agree        = el.querySelector( '[name="agree"]' ),
+            form_nonce        = el.querySelector( '[name="nonce"]' );
 
+        if ( form_company_name !== null ) {
+          data.company_name = form_company_name.value;
+        }
+        if ( form_nip !== null ) {
+          data.nip = form_nip.value;
+        }
         if ( form_first_name !== null ) {
           data.first_name = form_first_name.value;
+        }
+        if ( form_last_name !== null ) {
+          data.last_name = form_last_name.value;
         }
         if ( form_email !== null ) {
           data.email = form_email.value;
         }
-        if ( form_message_title !== null ) {
-          data.message_title = form_message_title.value;
+        if ( form_phone !== null ) {
+          data.phone = form_phone.value;
         }
         if ( form_message !== null ) {
           data.message = form_message.value;
+        }
+        if ( form_agree !== null ) {
+          data.agree = form_agree.checked ? 1 : 0;
         }
         if ( form_nonce !== null ) {
           data.nonce = form_nonce.value;
         }
 
-        window.grecaptcha.ready( function () {
-          window.grecaptcha
-                .execute( recaptcha_key, { action: "contact" } )
-                .then( function ( token ) {
-                  data.recaptcha = token;
-                  resolve( data );
-                } );
-        } );
+        if ( recaptcha_key !== '' ) {
+          window.grecaptcha.ready( function() {
+            window.grecaptcha
+                  .execute( recaptcha_key, { action: 'contact' } )
+                  .then( function( token ) {
+                    data.recaptcha = token;
+                    resolve( data );
+                  } );
+          } );
+        }
+        else {
+          resolve( data );
+        }
       } );
-    },
-    beforeSubmit: ( el ) => {
+    }, beforeSubmit: ( el ) => {
       let is_valid = pristine.validate();
       if ( is_valid ) {
-        loader.classList.add( "loader--show-in-form" );
-        let formResponse = contact_form.querySelector(
-          '[data-element="response"]'
-        );
-        formResponse.style.display = "flex";
+        loader.classList.add( 'loader--show-in-form' );
+        let formResponse = contact_form.querySelector( '[data-element="response"]' );
+        formResponse.style.display = 'flex';
       }
       else {
-        let top =
-          el.querySelector( ".has-danger" ).getBoundingClientRect().top +
-          window.pageYOffset;
+        let top = el.querySelector( '.has-danger' ).getBoundingClientRect().top + window.pageYOffset;
         //console.log(top);
-        window.scrollTo( { top: top - 100, behavior: "smooth" } );
+        window.scrollTo( { top: top - 100, behavior: 'smooth' } );
       }
       return is_valid;
-    },
-    success:      ( el, response ) => {
-      let formResponse = contact_form.querySelector(
-        '[data-element="response"]'
-      );
+    }, success:      ( el, response ) => {
+      let formResponse = contact_form.querySelector( '[data-element="response"]' );
       let formSuccess = contact_form.querySelector( '[data-response="success"]' );
-      loader.classList.remove( "loader--show-in-form" );
+      loader.classList.remove( 'loader--show-in-form' );
       formSuccess.innerText = response.message;
-      formSuccess.style.display = "block";
+      formSuccess.style.display = 'block';
       setTimeout( () => {
-        el.querySelector( '[name="first_name"]' ).value = "";
-        el.querySelector( '[name="email"]' ).value = "";
-        el.querySelector( '[name="message_title"]' ).value = "";
-        el.querySelector( '[name="message"]' ).value = "";
-        el.querySelector( "#daneOsobowe" ).checked = false;
-        formResponse.style.display = "none";
+        el.querySelector( '[name="company_name"]' ).value = '';
+        el.querySelector( '[name="nip"]' ).value = '';
+        el.querySelector( '[name="first_name"]' ).value = '';
+        el.querySelector( '[name="last_name"]' ).value = '';
+        el.querySelector( '[name="email"]' ).value = '';
+        el.querySelector( '[name="phone"]' ).value = '';
+        el.querySelector( '[name="message"]' ).value = '';
+        el.querySelector( '#daneOsobowe' ).checked = false;
+        formResponse.style.display = 'none';
       }, 6000 );
-    },
-    error:        ( el, response ) => {
+    }, error:        ( el, response ) => {
       //console.log(response);
-      let formResponse = contact_form.querySelector(
-        '[data-element="response"]'
-      );
+      let formResponse = contact_form.querySelector( '[data-element="response"]' );
       let formError = contact_form.querySelector( '[data-response="error"]' );
-      loader.classList.remove( "loader--show-in-form" );
+      loader.classList.remove( 'loader--show-in-form' );
       formError.innerText = response;
       setTimeout( () => {
-        formResponse.style.display = "none";
+        formResponse.style.display = 'none';
       }, 6000 );
-    }
+    },
   } );
 }
